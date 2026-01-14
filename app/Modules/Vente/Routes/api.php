39265275090221b0078b73cdcd9f17dd02fcc4d0
sylladev1805 <ousmanespatec@gmail.com>
2026-01-14 14:56:57 +0000

    activation de la fonctionalite active station pour le super admin

--- app/Modules/Vente/Routes/api.php
+++ app/Modules/Vente/Routes/api.php
@@ -8,7 +8,7 @@ use App\Modules\Vente\Controllers\ValidationVenteController;
 use App\Modules\Vente\Controllers\VenteLitreController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['station.db', 'auth:sanctum'])->prefix('v1/vente')->group(function () {
+Route::middleware(['station.db', 'auth:sanctum','active.st'])->prefix('v1/vente')->group(function () {
     Route::apiResource('cuves', ProduitController::class);
     //  Route::apiResource('ligne-ventes',LigneVenteController::class);
      Route::get(
