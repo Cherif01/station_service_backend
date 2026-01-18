26f4d987e14846a9a08ac01cdcfd066b9d2763d4
sylladev1805 <ousmanespatec@gmail.com>
2026-01-18 02:18:49 +0000

    mises a jour

--- app/Modules/Administration/Routes/api.php
+++ app/Modules/Administration/Routes/api.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('v1/admin')->middleware('station.db')->group(function () {
     Route::post('/login', [UserController::class, 'login']);
 
-    Route::middleware(['auth:sanctum','active.st'])->group(function () {
+    Route::middleware(['auth:sanctum'])->group(function () {
         Route::apiResource('users', UserController::class);
         Route::get('pompiste-dispo',[UserController::class,'pompisteDisp']);
         Route::apiResource('affectation', AffectationController::class);
