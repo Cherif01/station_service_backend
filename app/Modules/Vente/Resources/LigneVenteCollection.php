518c9ff0b0101f0c0f5493b19600d9ba60214762
sylladev1805 <ousmanespatec@gmail.com>
2026-03-12 14:24:06 +0000

    mises a jour

--- app/Modules/Vente/Resources/LigneVenteCollection.php
+++ app/Modules/Vente/Resources/LigneVenteCollection.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Modules\Vente\Resources;
 
 use Illuminate\Http\Request;
@@ -10,47 +9,51 @@ class LigneVenteCollection extends ResourceCollection
     public function toArray(Request $request): array
     {
         return $this->collection->map(function ($item) {
+            $qteVendu     = (float) $item->qte_vendu;
+            $prixUnitaire = (float) $item->prix_unitaire;
 
             return [
 
-                'id' => $item->id,
+                'id'            => $item->id,
 
-                'index_debut' => (float) $item->index_debut,
-                'index_fin'   => (float) $item->index_fin,
-                'qte_vendu'   => (float) $item->qte_vendu,
-                'retour_cuve' => (float) $item->retour_cuve,
+                'index_debut'   => (float) $item->index_debut,
+                'index_fin'     => (float) $item->index_fin,
+                'qte_vendu'     => (float) $item->qte_vendu,
+                'retour_cuve'   => (float) $item->retour_cuve,
+                'prix_unitaire' => $prixUnitaire,
+                'montant'       => $qteVendu * $prixUnitaire,
 
-                'status' => $item->status ? 'valider' : 'en cours',
+                'status'        => $item->status ? 'valider' : 'en cours',
 
-                'station' => $item->affectation?->pompe?->station
+                'station'       => $item->affectation?->pompe?->station
                     ? [
-                        'id'      => $item->affectation->pompe->station->id,
-                        'libelle' => $item->affectation->pompe->station->libelle,
-                    ]
+                    'id'      => $item->affectation->pompe->station->id,
+                    'libelle' => $item->affectation->pompe->station->libelle,
+                ]
                     : null,
 
-                'pompe' => $item->affectation?->pompe
+                'pompe'         => $item->affectation?->pompe
                     ? [
-                        'id'         => $item->affectation->pompe->id,
-                        'libelle'    => $item->affectation->pompe->libelle,
-                        'reference'  => $item->affectation->pompe->reference,
-                        'type_pompe' => $item->affectation->pompe->type_pompe,
-                    ]
+                    'id'         => $item->affectation->pompe->id,
+                    'libelle'    => $item->affectation->pompe->libelle,
+                    'reference'  => $item->affectation->pompe->reference,
+                    'type_pompe' => $item->affectation->pompe->type_pompe,
+                ]
                     : null,
 
-                'pompiste' => $item->affectation?->user
+                'pompiste'      => $item->affectation?->user
                     ? [
-                        'id'        => $item->affectation->user->id,
-                        'name'      => $item->affectation->user->name,
-                        'telephone' => $item->affectation->user->telephone,
-                    ]
+                    'id'        => $item->affectation->user->id,
+                    'name'      => $item->affectation->user->name,
+                    'telephone' => $item->affectation->user->telephone,
+                ]
                     : null,
 
-                'created_by' => $item->createdBy?->name,
-                'modify_by'  => $item->modifiedBy?->name,
+                'created_by'    => $item->createdBy?->name,
+                'modify_by'     => $item->modifiedBy?->name,
 
-                'created_at' => $item->created_at?->toDateTimeString(),
-                'updated_at' => $item->updated_at?->toDateTimeString(),
+                'created_at'    => $item->created_at?->toDateTimeString(),
+                'updated_at'    => $item->updated_at?->toDateTimeString(),
 
             ];
 
